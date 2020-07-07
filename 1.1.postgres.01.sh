c5bc584dec1bd99e91a23b39dc339daaf3a417be
Namsik Jung <nsjung74@gmail.com>
2020-07-07 15:42:57 +0900

    postgresql 다운로드 -y 옵션 추가

--- 1.1.postgres.01.sh
+++ 1.1.postgres.01.sh
@@ -14,7 +14,7 @@ echo "postgres ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/postgres
 # install postgres release rpm, this will add a file /etc/yum.repos.d/pgdg-${MAJORVER}-centos.repo
 ##--postgresql Download : centos7 : https://www.postgresql.org/download/linux/redhat/
 #yum install -y https://download.postgresql.org/pub/repos/yum/${MAJORVER}/redhat/rhel-7-x86_64/pgdg-centos${MAJORVER}-${MAJORVER}-${MINORVER}.noarch.rpm
-yum install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 # with the repo added (repo is called pgdg${MAJORVER}), ww can install postgres
 # NB: postgres is also available from Centos base repos, but we want the latest version (${MAJORVER})
 #yum install -y postgresql${MAJORVER}-${MAJORVER}.${MINORVER} postgresql${MAJORVER}-server-${MAJORVER}.${MINORVER}  postgresql${MAJORVER}-contrib-${MAJORVER}.${MINORVER}
